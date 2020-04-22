beda8e8b7f7de7ae97c6d41bc4c2679cb91b67ac
Shelby Polasek <shelbypolasek@gmail.com>
2020-04-22 15:48:51 -0500

    practice assessment

--- solutions.js
+++ solutions.js
@@ -2,14 +2,14 @@
 
 /**
  * Write your solutions here.
- *
- * **Note**: While normally it is good practice to wrap your javascript in an
+*
+/* * **Note**: While normally it is good practice to wrap your javascript in an
  * immediately invoked function expression (iife), you should _not_ do that
  * here. If you do, the automated tests will not be able to see your functions.
  * If you don't know what an iffe is, don't worry about it :)
  *
  * This file is for defining the following functions. See README.md for more detail.
- * isTrue
+  * isTrue
  * isFalse
  * not
  * addOne
@@ -20,3 +20,42 @@
  * and
  * concat
  */
+
+function isTrue(input){
+    return input === true;
+}
+
+function isFalse(input){
+    return input === false;
+}
+
+function not(input){
+    return !input;
+}
+
+function addOne(input) {
+    return (Number(input) + 1)
+}
+
+function isEven(input) {
+     if(input % 2 === 0){
+    return true;
+}else  {
+   return false;
+}
+}
+function isIdentical(input1, input2){
+    return input1 === input2;
+}
+function isEqual(input1, input2) {
+    return input1 == input2;
+}
+function or(input1, input2) {
+    return input1 || input2;
+}
+function and(input1, input2){
+    return input1 && input2;
+}
+function concat(input1, input2) {
+    return input1.concat(input2);
+}
