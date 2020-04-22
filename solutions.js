bbe59c9382b4ca80a51ecf67030fd2e77a5322ee
Jay Arredondo <jearredondo13@gmail.com>
2020-04-22 15:28:28 -0500

    finished with practice assessment

--- solutions.js
+++ solutions.js
@@ -1,5 +1,49 @@
 "use strict";
 
+function isTrue(input) {
+    return input === true;
+}
+
+function isFalse(input) {
+    return input === false;
+}
+
+function not(input) {
+    return !input === true;
+}
+
+function addOne(input) {
+    return Number(input) + 1;
+}
+
+function isEven(input) {
+    if (typeof input === "number" || typeof input === "string") {
+        return Number(input) % 2 === 0
+    } else {
+        return false;
+    }
+}
+
+function isIdentical(input1, input2) {
+    return input1 === input2;
+}
+
+function isEqual(input1, input2) {
+    return input1 == input2;
+}
+
+function or(input1, input2) {
+    return input1 || input2;
+}
+
+function and(input1, input2) {
+    return input1 && input2;
+}
+
+function concat(string1, string2) {
+    return String(string1) + String(string2);
+}
+
 /**
  * Write your solutions here.
  *
