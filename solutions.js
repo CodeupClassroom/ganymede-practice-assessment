b6a07543a5bb3d85025fdb4ddde2a82b0943b9a1
George Williams <george412williams@gmail.com>
2020-04-22 15:35:14 -0500

    first draft

--- solutions.js
+++ solutions.js
@@ -20,3 +20,89 @@
  * and
  * concat
  */
+
+// 1
+
+function isTrue(input) {
+    if (input === true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 2
+
+function isFalse(input) {
+    if (input === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 3
+
+function not(input) {
+    if (input == true) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// 4
+
+function addOne(input) {
+    if (input == "number") {
+        return input + 1;
+    }
+}
+
+// 5
+
+function isEven(input) {
+    if (input % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 6
+
+function isIdentical(input1, input2) {
+    if (input1 === input2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 7
+
+function isEqual() {
+    if (input1 == input2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 8
+
+function or(input1, input2) {
+    return input1 || input2;
+}
+
+// 9
+
+function and(input1, input2) {
+    return input1 && input2;
+}
+
+// 10
+
+function concat() {
+
+}
